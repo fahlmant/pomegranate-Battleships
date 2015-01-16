baaa5b1a8723fcf95fbad41473d3e66a5d02b2e7
fahlmant <fahlmantaylor@gmail.com>
2015-01-15 22:00:38 -0800

    Added comment 2

--- Battleships/src/edu/oregonstate/cs361/pomegranate/Ship.java
+++ Battleships/src/edu/oregonstate/cs361/pomegranate/Ship.java
@@ -3,8 +3,8 @@ package edu.oregonstate.cs361.pomegranate;
 public class Ship {
 	
 	public Ship(String kind) {
-//Team pomegranate, pair 1 was here
 	//TODO implement
+	//Pair 2 was here, team promegranate
 	}
 
 }
