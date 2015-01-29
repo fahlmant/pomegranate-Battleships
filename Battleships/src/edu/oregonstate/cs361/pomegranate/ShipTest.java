6aeb72b84de280bd2927f7ac7988effff6b37a16
nikhil24 <NikhilK@Nikhils-MacBook-Pro.local>
2015-01-28 22:15:26 -0800

    adding shiptest

---
+++ Battleships/src/edu/oregonstate/cs361/pomegranate/ShipTest.java
@@ -0,0 +1,26 @@
+package edu.oregonstate.cs361.pomegranate;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShipTest {
+	
+	private String Ship;
+	private String Minesweeper;
+	private String Battleship;
+	private String Destroyer;
+	
+
+	@Test
+	public void ShipName(){
+		if (Ship == Minesweeper) {
+		return;	
+		} else if (Ship == Battleship) {
+			return;
+		} else if (Ship == Destroyer) {
+			return;
+		}
+	}
+}
+
