f7cb0f41c3df1f2b61951d5909497b09146ede1b
nikhil24 <NikhilK@10-196-31-208.wireless.oregonstate.edu>
2015-01-29 18:10:01 -0800

    add ship name and health

--- Battleships/src/edu/oregonstate/cs361/pomegranate/ShipTest.java
+++ Battleships/src/edu/oregonstate/cs361/pomegranate/ShipTest.java
@@ -5,18 +5,45 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ShipTest {
-	
+
+	//Submarine
 	@Test
-	public void testShipKind() {
+	public void testShipSubmarine() {
 		Ship ship = new Ship("Submarine");
 		String k = ship.getKind();
 		assertEquals("Submarine", k);
-	}
-	
-	@Test
-	public void testShipLife() {
-		Ship ship = new Ship("Submarine");
 		ship.setHealth(3);
 		assertEquals(3, ship.getHealth());
 	}
+	
+	//Battleship
+	@Test
+	public void testShipBattleship() {
+		Ship ship = new Ship("Battleship");
+		String k = ship.getKind();
+		assertEquals("Battleship", k);
+		ship.setHealth(4);
+		assertEquals(4, ship.getHealth());
+	}
+	
+	//Destroyer 
+	@Test
+	public void testShipDestroyer() {
+		Ship ship = new Ship("Destroyer");
+		String k = ship.getKind();
+		assertEquals("Destroyer", k);
+		ship.setHealth(3);
+		assertEquals(3, ship.getHealth());
+	}
+	
+	//Minesweeper
+	@Test
+	public void testShipMinesweeper() {
+		Ship ship = new Ship("Minesweeper");
+		String k = ship.getKind();
+		assertEquals("Minesweeper", k);
+		ship.setHealth(2);
+		assertEquals(2, ship.getHealth());
+	}	
 }
+
