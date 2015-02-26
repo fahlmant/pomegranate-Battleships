56d10755265bd5337b4df8431da0959302d2080a
nikhil24 <NikhilK@10-196-25-198.wireless.oregonstate.edu>
2015-02-25 18:09:11 -0800

    pushing test for submarine

---
+++ Battleships/src/edu/oregonstate/cs361/pomegranate/SubmarineTest.java
@@ -0,0 +1,25 @@
+package edu.oregonstate.cs361.pomegranate;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SubmarineTest {
+
+	@Test
+	public void testPlaceShipVertical() {
+		Ship s = new Submarine("Sumbarine",'A', 1, true);
+		Board b = new Board();
+		b.placeShip(s);
+		assertEquals(b.getShip(0).getLocation().get(0).getX(), 'A');
+		assertEquals(b.getShip(0).getLocation().get(0).getY(), 1);
+		assertEquals(b.getShip(0).getLocation().get(1).getX(), 'A');
+		assertEquals(b.getShip(0).getLocation().get(1).getY(), 2);
+		assertEquals(b.getShip(0).getLocation().get(2).getX(), 'A');
+		assertEquals(b.getShip(0).getLocation().get(2).getY(), 3);
+		assertEquals(b.getShip(0).getLocation().get(3).getX(), 'A');
+		assertEquals(b.getShip(0).getLocation().get(3).getY(), 4);
+		assertEquals(b.getShip(0).getLocation().get(4).getX(), 'B');
+		assertEquals(b.getShip(0).getLocation().get(4).getY(), 2);
+	}
+}
