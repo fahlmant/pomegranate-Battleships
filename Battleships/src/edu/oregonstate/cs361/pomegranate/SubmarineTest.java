cbed8f3b650bc8f92aea05f82692a156b7561acc
Dylan Camus <camusd@onid.orst.edu>
2015-02-25 18:15:33 -0800

    fixed the sub test

--- Battleships/src/edu/oregonstate/cs361/pomegranate/SubmarineTest.java
+++ Battleships/src/edu/oregonstate/cs361/pomegranate/SubmarineTest.java
@@ -8,18 +8,18 @@ public class SubmarineTest {
 
 	@Test
 	public void testPlaceShipVertical() {
-		Ship s = new Submarine("Sumbarine",'A', 1, true);
+		Ship s = new Submarine("Sumbarine",'A', 10, true);
 		Board b = new Board();
 		b.placeShip(s);
 		assertEquals(b.getShip(0).getLocation().get(0).getX(), 'A');
-		assertEquals(b.getShip(0).getLocation().get(0).getY(), 1);
+		assertEquals(b.getShip(0).getLocation().get(0).getY(), 10);
 		assertEquals(b.getShip(0).getLocation().get(1).getX(), 'A');
-		assertEquals(b.getShip(0).getLocation().get(1).getY(), 2);
+		assertEquals(b.getShip(0).getLocation().get(1).getY(), 9);
 		assertEquals(b.getShip(0).getLocation().get(2).getX(), 'A');
-		assertEquals(b.getShip(0).getLocation().get(2).getY(), 3);
+		assertEquals(b.getShip(0).getLocation().get(2).getY(), 8);
 		assertEquals(b.getShip(0).getLocation().get(3).getX(), 'A');
-		assertEquals(b.getShip(0).getLocation().get(3).getY(), 4);
+		assertEquals(b.getShip(0).getLocation().get(3).getY(), 7);
 		assertEquals(b.getShip(0).getLocation().get(4).getX(), 'B');
-		assertEquals(b.getShip(0).getLocation().get(4).getY(), 2);
+		assertEquals(b.getShip(0).getLocation().get(4).getY(), 9);
 	}
 }
